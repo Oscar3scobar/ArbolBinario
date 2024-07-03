ad6484824448eb123b5e8d33821ab63c1f6e0316
Oscar Armando Escobar Rojas <heddy4132@gmail.com>
2024-07-02 23:50:22 -0600

    Primer commit

---
+++ BSTWebApp/Controllers/ArbolController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using BSTWebApp.Models;
+
+namespace BSTWebApp.Controllers
+{
+    public class ArbolController : Controller
+    {
+        private static ArbolBinario<int> arbol = new ArbolBinario<int>();
+        public ActionResult Index(string mensaje)
+        {
+            ViewBag.Mensaje = mensaje;
+            ViewBag.ResultadoBusqueda = null;
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Insertar(int numero)
+        {
+            arbol.Insertar(numero);
+
+            return RedirectToAction("Index", new { mensaje = "Número insertado correctamente en el árbol." });
+        }
+
+    }
+}
---
+++ BSTWebApp/Models/Arbol.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BSTWebApp.Models
+{
+    public class Nodo<T> where T : IComparable
+    {
+        public T Valor { get; set; }
+        public Nodo<T> Izquierdo { get; set; }
+        public Nodo<T> Derecho { get; set; }
+
+        public Nodo(T valor)
+        {
+            Valor = valor;
+            Izquierdo = null;
+            Derecho = null;
+        }
+    }
+
+    public class ArbolBinario<T> where T : IComparable
+    {
+        public Nodo<T> Raiz { get; private set; }
+
+        public ArbolBinario()
+        {
+            Raiz = null;
+        }
+
+        public void Insertar(T valor)
+        {
+            Raiz = InsertarRecursivo(Raiz, valor);
+        }
+
+        private Nodo<T> InsertarRecursivo(Nodo<T> nodo, T valor)
+        {
+            if (nodo == null)
+            {
+                return new Nodo<T>(valor);
+            }
+
+            if (valor.CompareTo(nodo.Valor) < 0)
+            {
+                nodo.Izquierdo = InsertarRecursivo(nodo.Izquierdo, valor);
+            }
+            else
+            {
+                nodo.Derecho = InsertarRecursivo(nodo.Derecho, valor);
+            }
+
+            return nodo;
+        }
+
+    }
+}
